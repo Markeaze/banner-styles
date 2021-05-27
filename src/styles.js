a932118840c705d3e0445e25c626108d1020acb2
Svetlana Zorina <svetlana.olist@gmail.com>
2021-05-27 09:05:54 +0300

    Init styles

---
+++ src/styles.js
@@ -0,0 +1,4 @@
+import base from "./style/base";
+import types from "./style/types";
+import teaser from "./style/teaser";
+import animations from "./style/animations";
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+const isProduction = true;
+
+module.exports = {
+  name: "styles",
+  entry: ["./src/styles.js"],
+  output: {
+    filename: "[name]",
+    path: path.resolve("dist"),
+    publicPath: "/",
+  },
+  optimization: {
+    namedModules: true,
+    namedChunks: true,
+    moduleIds: "named",
+    chunkIds: "named",
+    portableRecords: true,
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [{ loader: "babel-loader" }],
+      },
+      {
+        test: /\.s?(a|c)ss$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: !isProduction,
+              publicPath: (resourcePath, context) => {
+                // publicPath is the relative path of the resource to the context
+                // e.g. for ./css/admin/main.css the publicPath will be ../../
+                // while for ./css/main.css the publicPath will be ../
+                return path.relative(path.dirname(resourcePath), context) + "/";
+              },
+            },
+          },
+          { loader: "css-loader" },
+          { loader: "sass-loader" },
+        ],
+      },
+    ],
+  },
+  watch: false,
+  watchOptions: {
+    ignored: ["node_modules/*"],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".scss"],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      splitChunks: false,
+      filename: "styles.min.css",
+      chunkFileName: "[id].css",
+      ignoreOrder: false,
+    }),
+  ],
+};
